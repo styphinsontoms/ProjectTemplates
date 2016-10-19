8ba8d1c5c5149870cf8f6f3b21a6dca75704bd68
styphinsontoms <styphinsontoms@gmail.com>
2016-10-19 10:48:33 +0530

    Added maven core java template project

---
+++ MavenCoreJavaTemplate/src/main/java/com/example/corejavatemplate/App.java
@@ -0,0 +1,14 @@
+package com.example.corejavatemplate;
+
+
+/**
+ * Generate a unique number
+ *
+ */
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Date : " + new java.util.Date());
+	}
+
+}
---
+++ MavenCoreJavaTemplate/src/test/java/com/example/corejavatemplate/AppTest.java
@@ -0,0 +1,13 @@
+package com.example.corejavatemplate;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void testLengthOfTheUniqueKey() {
+
+		System.out.println("Default Test template");
+
+	}
+}
