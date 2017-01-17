35d49d90b4ca07821170fe94eb7834804b0fdb9b
styphinsontoms <styphinsontoms@gmailcom>
2017-01-17 13:37:35 +0530

    added maven templates

---
+++ MavenFromScratch/ExampleJar/exampleJar/src/main/java/com/shaninfo/exampleJar/App.java
@@ -0,0 +1,18 @@
+package com.shaninfo.exampleJar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+	
+public static String now()
+{
+return new java.util.Date().toString();
+}
+}
---
+++ MavenFromScratch/ExampleJar/exampleWebApp/src/main/webapp/index.jsp
@@ -0,0 +1,9 @@
+<html>
+<body>
+<center>
+<h2>Fancy Clock Application!</h2>
+<h1>This will display the time now </h1>
+</center>
+<%=com.shaninfo.exampleJar.App.now()%>
+</body>
+</html>
